909bb55bc769dbb35287cf18bf3c71a3451d8410
Jayaran N <jayaraj652003@gmail.com>
2025-08-03 11:14:12 +0530

    Plot graph

---
+++ pyplot/plot/demo4.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = {
+    'cricket_bat': ['SG', 'BDM', 'SS', 'GM', 'Kookaburra', 'Spartan'],
+    'mrp': [2000, 2200, 2400, 2700, 2800, 3000],
+    'weight_grams': [1100, 1200, 1250, 1330, 1480, 1600]
+}
+
+df = pd.DataFrame(data)
+
+plt.plot(df['mrp'], df['weight_grams'])
+plt.xlabel('Bat Price (USD)')
+plt.ylabel('Bat Weight (Grams)')
+plt.grid()
+plt.show()
---
+++ pyplot/plot/demo8.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = {
+    'cricket_bat': ['SG', 'BDM', 'SS', 'GM', 'Kookaburra', 'Spartan'],
+    'mrp_jan': [2000, 2200, 2400, 2700, 2800, 3000],
+    'mrp_feb': [2100, 2300, 2500, 2800, 2900, 3100],
+    'weight_grams': [1100, 1200, 1250, 1330, 1480, 1600]
+}
+
+df = pd.DataFrame(data)
+
+fig = plt.figure()
+
+ax = plt.subplot()
+ax.plot(df['weight_grams'], df['mrp_jan'], '--', label='January')
+ax.plot(df['weight_grams'], df['mrp_feb'], ':', label='February')
+
+legend = ax.legend(loc='lower right', fontsize='large')
+legend.get_frame().set_facecolor('none')
+
+plt.xlabel('Bat Weight (Grams)')
+plt.ylabel('Bat Price (MRP)')
+plt.title('Bat price depends on the weight')
+plt.show()
